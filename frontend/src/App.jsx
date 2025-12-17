e25dbecbaf02810945efa1100ed684aee9d1a62d
Sagar Bagalkot <bagalkotsagar@gmail.com>
2025-12-17 09:22:20 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,11 @@
 import React, { useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import Navbar from "./Components/Navbar";
-import Home from "./Pages/Home";
-import Login from "./Pages/Login";
-import Signup from "./Pages/Signup";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
 import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   const [searchQuery, setSearchQuery] = useState("");
@@ -12,13 +13,15 @@ function App() {
   return (
     <>
       <Navbar onSearch={setSearchQuery} />
+
       <Routes>
         <Route path="/" element={<Home searchQuery={searchQuery} />} />
         <Route path="/home" element={<Home searchQuery={searchQuery} />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
       </Routes>
-      <ToastContainer position="top-right" />
+
+      <ToastContainer position="top-right" autoClose={2000} />
     </>
   );
 }
